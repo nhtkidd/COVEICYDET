5b12738cfadabc81ec63767f55a045d25ccb9144
Jesus M <jesusmdz929@gmail.com>
2022-09-16 17:00:26 -0500

    regex for name and last name input

--- app/Http/Requests/registerRequest.php
+++ app/Http/Requests/registerRequest.php
@@ -25,8 +25,8 @@ class registerRequest extends FormRequest
     {
 
         return [
-            'name' => 'required|max:50|regex:/^[a-zA-ZÑñ\s]+$/',
-            'last_name' => 'required|max:50|regex:/^[a-zA-ZÑñ\s]+$/',
+            'name' => 'required|max:50|regex:/^[a-zA-ZÑñáéíóúÁÉÍÓÚ\s]+$/',
+            'last_name' => 'required|max:50|regex:/^[a-zA-ZÑñáéíóúÁÉÍÓÚ\s]+$/',
             'curp' =>  [
                 'required',
                 'regex:/^([A-Z][AEIOUX][A-Z]{2}\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])[HM](?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)[B-DF-HJ-NP-TV-Z]{3}[A-Z\d])(\d)$/',
